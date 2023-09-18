cc6c75864a4976af966982ac14ddc657e244a7b5
dengzhiwei <2656392425@qq.com>
2023-09-18 22:22:55 +0800

    feat: 人气和新鲜模块搭建及图片懒加载实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -6,3 +6,15 @@ export function getBannerAPI() {
     url: '/home/banner'
   })
 }
+
+export function findNewAPI(){
+    return httpInstance({
+        url:'/home/new'
+    })
+}
+
+export function findHotAPI(){
+    return httpInstance({
+        url:'/home/hot'
+    })
+}
