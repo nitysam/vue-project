bd2000a91b57ae7af66044ef427bbf227bf9945c
dengzhiwei <2656392425@qq.com>
2023-09-20 20:48:29 +0800

    feat：激活状态显示，分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -8,25 +8,75 @@
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="" />
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+      <!-- 主体 -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { getCategoryAPI } from '@/apis/category';
-import { onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { getCategoryAPI } from '@/apis/category'
+import { getBannerAPI } from '@/apis/home'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import GoodsItem from '../Home/components/GoodsItem.vue'
 //获取数据
 const categoryData = ref({})
 const route = useRoute()
-const getCategory = async ()=>{
+const getCategory = async () => {
   const res = await getCategoryAPI(route.params.id)
   categoryData.value = res.result
 }
-onMounted(()=> getCategory())
+//轮播图
+const bannerList = ref([])
+const getBanner = async () => {
+  const res = await getBannerAPI({ distributionSite: 2 })
+  console.log(res)
+  bannerList.value = res.result
+}
+onMounted(() => {
+  getCategory()
+  getBanner()
+})
 </script>
 
 <style scoped lang="scss">
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
 .top-category {
   h3 {
     font-size: 28px;
@@ -48,7 +98,6 @@ onMounted(()=> getCategory())
       li {
         width: 168px;
         height: 160px;
-
 
         a {
           text-align: center;
