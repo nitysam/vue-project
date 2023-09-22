dbb20a813465a804d0de302eb7daa47f0a8a5155
dengzhiwei <2656392425@qq.com>
2023-09-22 23:41:37 +0800

    feat: 放大镜-滑块跟随移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,45 +1,86 @@
-<script setup>
-import {ref} from 'vue'
-// 图片列表
-const imageList = [
-  "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
-  "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
-  "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
-  "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
-  "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
-]
-const activeIndex = ref(0)
-const enterHandler = (i)=>{
-    activeIndex.value = i
-}
-</script>
-
-
 <template>
+  {{ elementX }},{{ elementY }},{{ isOutside }}
   <div class="goods-image">
     <!-- 左侧大图-->
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
-      <li v-for="(img, i) in imageList" :key="i" @mouseenter="enterHandler(i)" :class="{active: i === activeIndex}">
+      <li
+        v-for="(img, i) in imageList"
+        :key="i"
+        @mouseenter="enterHandler(i)"
+        :class="{ active: i === activeIndex }"
+      >
         <img :src="img" alt="" />
       </li>
     </ul>
     <!-- 放大镜大图 -->
-    <div class="large" :style="[
-      {
-        backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
-      },
-    ]" v-show="false"></div>
+    <div
+      class="large"
+      :style="[
+        {
+          backgroundImage: `url(${imageList[0]})`,
+          backgroundPositionX: `0px`,
+          backgroundPositionY: `0px`
+        }
+      ]"
+      v-show="false"
+    ></div>
   </div>
 </template>
+<script setup>
+import { ref, watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
+// 图片列表
+const imageList = [
+  'https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png',
+  'https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg',
+  'https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg',
+  'https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg',
+  'https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg'
+]
+const activeIndex = ref(0)
+const enterHandler = (i) => {
+  activeIndex.value = i
+}
+//获取鼠标相对位置
+const target = ref(null)
+const { elementX, elementY ,isOutside} = useMouseInElement(target)
 
+//控制滑块跟随鼠标移动
+const left = ref(0)
+const top = ref(0)
+watch([elementX, elementY], () => {
+  console.log('xy变化了')
+  //有效范围内控制滑块距离
+  //横向
+  if (elementX.value >= 100 && elementX.value <= 300) {
+    left.value = elementX.value - 100
+  }
+  //纵向
+  if (elementY.value >= 100 && elementY.value <= 300) {
+    top.value = elementY.value - 100
+  }
+  //处理边界
+  if (elementX.value > 300) {
+    left.value = 200
+  }
+  if (elementX.value < 100) {
+    left.value = 0
+  }
+
+  if (elementY > 300) {
+    top.value = 200
+  }
+  if (elementY < 100) {
+    top.value = 0
+  }
+})
+</script>
 <style scoped lang="scss">
 .goods-image {
   width: 480px;
