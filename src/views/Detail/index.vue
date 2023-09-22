4f95ad12fe1e859233e5c6b381eb79b0368784d9
dengzhiwei <2656392425@qq.com>
2023-09-22 17:45:01 +0800

    feat: 详情热榜基础组件封装

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -103,7 +103,12 @@
               </div>
             </div>
             <!-- 24热榜+专题推荐 -->
-            <div class="goods-aside"></div>
+            <div class="goods-aside">
+              <!-- 24小时 -->
+            <DetailHot/>
+            <!--周 -->
+            <DetailHot/>
+            </div>
           </div>
         </div>
       </div>
@@ -115,6 +120,7 @@
 import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import DetailHot from './components/DetailHot.vue'
 const route = useRoute()
 const goods = ref({})
 const getGoods = async () => {
