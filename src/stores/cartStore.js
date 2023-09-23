b6695069fe12c861353220ff63e5c56cd5b73bfb
dengzhiwei <2656392425@qq.com>
2023-09-23 20:33:47 +0800

    feat: 头部购物车-删除功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,21 +1,35 @@
 //封装购物车模块
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-export const useCartStore = defineStore('cart', () => {
-  const cartList = ref([])
-  const addCart = (goods) => {
-    //添加购物车操作
-    const item = cartList.value.find((item) => goods.skuId === item.skuId)
-    if (item) {
-      item.count++
-    } else {
-      cartList.value.push(goods)
+export const useCartStore = defineStore(
+  'cart',
+  () => {
+    const cartList = ref([])
+    const addCart = (goods) => {
+      //添加购物车操作
+      const item = cartList.value.find((item) => goods.skuId === item.skuId)
+      if (item) {
+        item.count++
+      } else {
+        cartList.value.push(goods)
+      }
     }
+    //splice方法
+    //   const delCart = (skuId) =>{
+    //    const idx =  cartList.value.findIndex((item)=>skuId === item.skuId)
+    //    cartList.value.splice(idx,1)
+    //   }
+    //filter方法
+    const delCart = (skuId) => {
+      cartList.value = cartList.value.filter((item) => skuId !== item.skuId)
+    }
+    return {
+      cartList,
+      addCart,
+      delCart
+    }
+  },
+  {
+    persist: true
   }
-  return {
-    cartList,
-    addCart
-  }
-},{
-    persist:true
-})
+)
