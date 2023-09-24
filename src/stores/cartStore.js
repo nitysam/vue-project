5b8c84bc84369a0e7d1115455437538fedc1829b
dengzhiwei <2656392425@qq.com>
2023-09-24 16:09:06 +0800

    feat: 购物车列表统计计算

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -41,11 +41,17 @@ export const useCartStore = defineStore(
     const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
     
     const isAll = computed(()=>cartList.value.every((item)=>item.selected))
+
+    const selectedCount = computed(()=>cartList.value.filter(item=>item.selected).reduce((a, c) => a + c.count, 0))
+
+    const selectedPrice = computed(()=>cartList.value.filter(item=>item.selected).reduce((a, c) => a + c.count*c.price, 0))
     return {
       cartList,
       allCount,
       allPrice,
       isAll,
+      selectedCount,
+      selectedPrice,
       addCart,
       delCart,
       singleCheck,
