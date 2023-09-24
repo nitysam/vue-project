8ae45f91e547ab88e89bd8c47b5a2697e5b2bbaf
dengzhiwei <2656392425@qq.com>
2023-09-24 16:28:57 +0800

    feat: 接口购物车-加入购物车实现

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,17 +1,29 @@
 //封装购物车模块
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
+import { useUserStore } from './user'
+import { insertCartAPI, findNewCartListAPI } from '@/apis/cart'
 export const useCartStore = defineStore(
   'cart',
   () => {
+    const userStore = useUserStore()
+    const isLogin = computed(() => userStore.userInfo.token)
     const cartList = ref([])
-    const addCart = (goods) => {
-      //添加购物车操作
-      const item = cartList.value.find((item) => goods.skuId === item.skuId)
-      if (item) {
-        item.count++
+    const addCart = async (goods) => {
+      const { skuId, count } = goods
+      if (isLogin.value) {
+        //登录之后的加入购物车逻辑
+        await insertCartAPI({ skuId, count })
+        const res = findNewCartListAPI()
+        cartList.value = res.result
       } else {
-        cartList.value.push(goods)
+        //添加到本地购物车操作
+        const item = cartList.value.find((item) => goods.skuId === item.skuId)
+        if (item) {
+          item.count++
+        } else {
+          cartList.value.push(goods)
+        }
       }
     }
     //splice方法
@@ -25,26 +37,30 @@ export const useCartStore = defineStore(
     }
 
     //单选功能
-    const singleCheck = (skuId,selected) =>{
+    const singleCheck = (skuId, selected) => {
       //通过skuId找到要修改的项 然后把它的selected改为传过来的selected
-      const item = cartList.value.find((item)=> item.skuId === skuId)
+      const item = cartList.value.find((item) => item.skuId === skuId)
       item.selected = selected
     }
     //全选功能
-    const allCheck = (selected) =>{
+    const allCheck = (selected) => {
       //把cartList中的每一项的seleced都设置为当前的全选框状态
-      cartList.value.forEach(item=>item.value.selected = selected)
+      cartList.value.forEach((item) => (item.value.selected = selected))
     }
 
     const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
 
     const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
-    
-    const isAll = computed(()=>cartList.value.every((item)=>item.selected))
 
-    const selectedCount = computed(()=>cartList.value.filter(item=>item.selected).reduce((a, c) => a + c.count, 0))
+    const isAll = computed(() => cartList.value.every((item) => item.selected))
 
-    const selectedPrice = computed(()=>cartList.value.filter(item=>item.selected).reduce((a, c) => a + c.count*c.price, 0))
+    const selectedCount = computed(() =>
+      cartList.value.filter((item) => item.selected).reduce((a, c) => a + c.count, 0)
+    )
+
+    const selectedPrice = computed(() =>
+      cartList.value.filter((item) => item.selected).reduce((a, c) => a + c.count * c.price, 0)
+    )
     return {
       cartList,
       allCount,
