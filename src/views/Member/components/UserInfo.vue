655a9c1afdc4859ff34cae20368396b479067128
dengzhiwei <2656392425@qq.com>
2023-09-25 10:51:03 +0800

    feat: 个人中心-会员信息渲染

--- src/views/Member/components/UserInfo.vue
+++ src/views/Member/components/UserInfo.vue
@@ -1,5 +1,21 @@
 <script setup>
-const userStore = {}
+import {useUserStore} from '@/stores/userStore'
+import {getLikeListAPI} from '@/apis/user'
+import { onMounted, ref } from 'vue'
+import GoodsItem from '@/views/Home/components/GoodsItem.vue'
+
+// 导入GoodsItem组件
+const userStore = useUserStore()
+
+// 获取猜你喜欢列表
+const likeList = ref([])
+
+const getLikeList = async () => {
+  const res = await getLikeListAPI({ limit: 4 })
+  likeList.value = res.result
+}
+
+onMounted(() => getLikeList())
 </script>
 
 <template>
@@ -32,7 +48,7 @@ const userStore = {}
         <h4 data-v-bcb266e0="">猜你喜欢</h4>
       </div>
       <div class="goods-list">
-        <!-- <GoodsItem v-for="good in likeList" :key="good.id" :good="good" /> -->
+        <GoodsItem v-for="good in likeList" :goods = 'good' :key="good.id"/>
       </div>
     </div>
   </div>
